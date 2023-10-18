5a92e5f7ee1cf3f40ca6d8c0466f1b038d034bef
Ted <tadeus.frederick@gmail.com>
2023-10-19 03:15:33 +0800

    add modal for button click on BrowseDAT

--- frontend/src/pages/BrowseDAT.tsx
+++ frontend/src/pages/BrowseDAT.tsx
@@ -7,8 +7,22 @@ import MiladyMaker from "../../public/MiladyMaker.jpeg";
 import CryptoPunks from "../../public/CryptoPunks.png";
 import Nouns from "../../public/nouns.webp";
 import PudgyPenguins from "../../public/PudgyPenguins.jpeg";
+import ModalBrowse from "../components/Modal/ModalBrowse";
+import { useDisclosure } from "@chakra-ui/react";
 
 function BrowseDAT() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  function handleCouncilClick() {
+    console.log("Council was clicked.");
+    // Do more things if needed
+  }
+
+  function handleVoterClick() {
+    console.log("Voter was clicked.");
+    // Do more things if needed
+  }
+
   return (
     <div className="h-[3000px] bg-gradient-to-r from-rose-200 to-teal-200">
       <Navbar />
@@ -27,12 +41,35 @@ function BrowseDAT() {
         </button>
       </div>
       <div className="m-8 container w-3/4 mx-auto grid grid-cols-3 gap-8 justify-center items-center">
-        <BrowseDATCard imgLink={BoredApe} daoName="Bored Ape Yacht Club" />
-        <BrowseDATCard imgLink={Azuki} daoName="Azuki" />
-        <BrowseDATCard imgLink={MiladyMaker} daoName="Milady Maker" />
-        <BrowseDATCard imgLink={CryptoPunks} daoName="CryptoPunks" />
-        <BrowseDATCard imgLink={Nouns} daoName="Nouns" />
-        <BrowseDATCard imgLink={PudgyPenguins} daoName="Pudgy Penguins" />
+        <BrowseDATCard
+          imgLink={BoredApe}
+          daoName="Bored Ape Yacht Club"
+          onClickButton={onOpen}
+        />
+        <ModalBrowse
+          DATName="Bored Ape Yacht Club"
+          OnClickCouncil={handleCouncilClick}
+          OnClickVoter={handleVoterClick}
+          isOpenModal={isOpen}
+          onCloseModal={onClose}
+        />
+        <BrowseDATCard imgLink={Azuki} daoName="Azuki" onClickButton={onOpen} />
+        <BrowseDATCard
+          imgLink={MiladyMaker}
+          daoName="Milady Maker"
+          onClickButton={onOpen}
+        />
+        <BrowseDATCard
+          imgLink={CryptoPunks}
+          daoName="CryptoPunks"
+          onClickButton={onOpen}
+        />
+        <BrowseDATCard imgLink={Nouns} daoName="Nouns" onClickButton={onOpen} />
+        <BrowseDATCard
+          imgLink={PudgyPenguins}
+          daoName="Pudgy Penguins"
+          onClickButton={onOpen}
+        />
       </div>
     </div>
   );
